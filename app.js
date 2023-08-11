a379808bbaa7a585d8ec1ab9616986953e5c1c62
Jhon Esparragoza <jhonantonyesp@gmail.com>
2023-08-10 21:52:17 -0400

    Cambios para funcionar con mongodb

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 import Express from "express";
 import bodyParser from "body-parser";
-
+import mongoose from "mongoose";
 const app = Express();
 app.use(Express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
 
-var items = [];
+async function main(){
+  await mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+  console.log("Conectado a la BD");
+}
+main();
 
-app.get("/", (req, res) => {
+//Parte de mongodb
+const taskSchema = new mongoose.Schema({
+  task: String
+})
+
+const Item = mongoose.model("Item", taskSchema);
+
+var Items = [];
+//Mongodb
+
+
+app.get("/", async (req, res) => {
   var today = new Date();
 
   var options = {
@@ -17,17 +31,24 @@ app.get("/", (req, res) => {
     month: "long"
   }
 
+  await Item.find().then(function(items){
+    Items = items;
+  })
+
   var day = today.toLocaleDateString("es-ES", options);
 
-  res.render("list", { 
+  res.render("list.ejs", { 
     KindOfDay: day ,
-    newListItems: items
+    newListItems: Items
   });
 });
 
 app.post("/", (req, res) =>{
     var item = req.body.newItem;
-    items.push(item);
+    let userItem = new Item ({
+      task: item
+    })
+    userItem.save();
     res.redirect("/");
 })
 
